1a4a72172c607a71468f374aa50b6a1f30a85440
theMatth33w <matheus.ibrahim@hotmail.com>
2020-06-09 13:32:06 -0300

    Deploy novo

---
+++ build/precache-manifest.cebf4d565e35f4fe4197fa7053d68277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82d8ee07991ceabc5919265b91a17aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d73794ff45e35089e6",
+    "url": "/static/css/main.a9083df9.chunk.css"
+  },
+  {
+    "revision": "edb405074bbe8d14f462",
+    "url": "/static/js/2.36bd5538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36bd5538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d73794ff45e35089e6",
+    "url": "/static/js/main.6d7c77e0.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5b242259bb933521b55816b5023f562b",
+    "url": "/static/media/background.5b242259.jpg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/build"));
+
+app.get("*", (request, response) => {
+    response.sendFile(__dirname + "/build/index.html")
+});
+
+app.listen(port);
